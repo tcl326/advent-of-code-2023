124a4a3f87ebf1b90db37e7e966a8a896fde437d
tcl326 <lintingche2012@gmail.com>
2023-12-02 15:07:32 +0800

    day 2 down

---
+++ d2-cube-conundrum.py
@@ -0,0 +1,127 @@
+"""
+--- Day 2: Cube Conundrum ---
+You're launched high into the atmosphere! The apex of your trajectory just barely reaches the surface of a large island floating in the sky. You gently land in a fluffy pile of leaves. It's quite cold, but you don't see much snow. An Elf runs over to greet you.
+
+The Elf explains that you've arrived at Snow Island and apologizes for the lack of snow. He'll be happy to explain the situation, but it's a bit of a walk, so you have some time. They don't get many visitors up here; would you like to play a game in the meantime?
+
+As you walk, the Elf shows you a small bag and some cubes which are either red, green, or blue. Each time you play this game, he will hide a secret number of cubes of each color in the bag, and your goal is to figure out information about the number of cubes.
+
+To get information, once a bag has been loaded with cubes, the Elf will reach into the bag, grab a handful of random cubes, show them to you, and then put them back in the bag. He'll do this a few times per game.
+
+You play several games and record the information from each game (your puzzle input). Each game is listed with its ID number (like the 11 in Game 11: ...) followed by a semicolon-separated list of subsets of cubes that were revealed from the bag (like 3 red, 5 green, 4 blue).
+
+For example, the record of a few games might look like this:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, three sets of cubes are revealed from the bag (and then put back again). The first set is 3 blue cubes and 4 red cubes; the second set is 1 red cube, 2 green cubes, and 6 blue cubes; the third set is only 2 green cubes.
+
+The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
+
+In the example above, games 1, 2, and 5 would have been possible if the bag had been loaded with that configuration. However, game 3 would have been impossible because at one point the Elf showed you 20 red cubes at once; similarly, game 4 would also have been impossible because the Elf showed you 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
+
+Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+
+--- Part Two ---
+The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
+
+As you continue your walk, the Elf poses a second question: in each game you played, what is the fewest number of cubes of each color that could have been in the bag to make the game possible?
+
+Again consider the example games from earlier:
+
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+In game 1, the game could have been played with as few as 4 red, 2 green, and 6 blue cubes. If any color had even one fewer cube, the game would have been impossible.
+Game 2 could have been played with a minimum of 1 red, 3 green, and 4 blue cubes.
+Game 3 must have been played with at least 20 red, 13 green, and 6 blue cubes.
+Game 4 required at least 14 red, 3 green, and 15 blue cubes.
+Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
+The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
+
+For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+"""
+from typing import Dict, Tuple, List
+
+import functools
+
+
+def parse_set(line: str) -> Dict[str, int]:
+    line = line.strip()
+    cube_list = line.split(",")
+    set = {}
+    for cube_str in cube_list:
+        count, cube_name = cube_str.split()
+        set[cube_name.strip()] = int(count)
+    
+    return set
+
+
+
+def parse_line(line: str) -> Tuple[int, List[Dict[str, int]]]:
+    assert line.startswith("Game")
+
+    id_str, game_str = line.split(":")
+    id_str = id_str[len("Game "):]
+    
+    set_list = game_str.split(";")
+    sets = []
+    for set_ in set_list:
+        sets.append(parse_set(set_))
+
+    return int(id_str), sets
+
+
+def is_possible(game: List[Dict[str, int]], bag: Dict[str, int]) -> bool:
+    for set_ in game:
+        for k, v in bag.items():
+            if v < set_.get(k, 0):
+                return False
+    return True
+
+def get_power(game: List[Dict[str, int]]) -> int:
+    min_bag = {
+        "red": 0,
+        "blue": 0,
+        "green": 0,
+    }
+    for set_ in game:
+        for k, v in set_.items():
+            min_bag[k] = max(min_bag[k], v)
+    return functools.reduce(lambda a, b: a * b, min_bag.values())
+
+
+def part1(games: Dict[int, List[Dict[str, int]]], bag: Dict[str, int]) -> int:
+    s = 0
+    for game_id, game in games.items():
+        if is_possible(game, bag):
+            s += game_id
+    return s
+
+
+def part2(games: Dict[int, List[Dict[str, int]]]) -> int:
+    s = 0
+    for game in games.values():
+        p = get_power(game)
+        s += p
+    return s
+
+
+if __name__ == "__main__":
+    file_name = "d2-input.text"
+
+    games = {}
+    
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            game_id, game = parse_line(line)
+            games[game_id] = game
+    
+    print(part1(games, {"red": 12, "green": 13, "blue": 14}))
+    print(part2(games)) 
